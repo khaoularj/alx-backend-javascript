37c191a8e70156f6ec4919790eff0f0a0bfa650d
khaoularj <khaoula.raja1@gmail.com>
2024-04-08 14:14:41 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false; // Use let instead of var
-  const task2 = true; // Use let instead of var
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // Use let instead of var
-    const task2 = false; // Use let instead of var
   }
 
   return [task, task2];
