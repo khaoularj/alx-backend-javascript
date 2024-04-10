ce744369548d697ff3bc24b1906058cb481856f6
khaoularj <khaoula.raja1@gmail.com>
2024-04-10 13:30:10 +0000

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch((error) => {
-      console.error("Signup system offline");
+    .catch(() => {
+      console.error('Signup system offline');
     });
 }
