2afeb0deea35a52b46ecdb78aeb1da8d0e6bdfb9
khaoularj <khaoula.raja1@gmail.com>
2024-04-10 13:36:26 +0000

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
-import signUpUser from "./4-user-promise.js";
-import uploadPhoto from "./5-photo-reject.js";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
-    signUpUser(firstName, lastName).then(value => ({ status: 'fulfilled', value })),
-    uploadPhoto(fileName).then(error => ({ status: 'rejected', value: error }))
+    signUpUser(firstName, lastName).then((value) => ({ status: 'fulfilled', value })),
+    uploadPhoto(fileName).then((error) => ({ status: 'rejected', value: error })),
   ];
 
   return Promise.all(promises);
