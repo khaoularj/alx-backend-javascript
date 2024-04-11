b2b330248bdbe9287122abb49ae36f09fc0af867
khaoularj <khaoula.raja1@gmail.com>
2024-04-11 15:54:31 +0000

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(nvAmount) {
+    this._amount = nvAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(nvCurrency) {
+    this._currency = nvCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
