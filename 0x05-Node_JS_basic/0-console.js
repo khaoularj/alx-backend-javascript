11f405685e30c91da5b325b30801071ac66705fe
khaoularj <khaoula.raja1@gmail.com>
2024-06-04 07:06:42 -0700

    correcting eslint

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 // function named displayMessage that prints in STDOUT the string argument
 
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
 module.exports = displayMessage;
