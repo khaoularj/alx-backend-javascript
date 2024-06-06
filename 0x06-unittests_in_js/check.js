d957632446a09ae47dd6ab66052a24ef25d9f87a
khaoularj <khaoula.raja1@gmail.com>
2024-06-06 11:52:30 +0000

    Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/check.js
@@ -0,0 +1,8 @@
+// check.js
+const calculateNumber = require('./0-calcul.js');
+
+console.log(calculateNumber(1, 3));      // Expected output: 4
+console.log(calculateNumber(1, 3.7));    // Expected output: 5
+console.log(calculateNumber(1.2, 3.7));  // Expected output: 5
+console.log(calculateNumber(1.5, 3.7));  // Expected output: 6
+
